afe28abbcf4f9e321e06e184c42a0fe66f91b655
James Moore <69149348+jkwmoore@users.noreply.github.com>
2021-09-23 20:41:22 +0100

    Update test-processing.sh

--- test-processing.sh
+++ test-processing.sh
@@ -2,7 +2,7 @@
 cd /structure-nst/
 th fast_neural_style.lua   -model trained_models/feathers.t7   -input_image images/content/model2.jpg  -output_image out.png
 MD5SUM=($(md5sum out.png))
-MD5SUMORIG=asdasdasdasda
+MD5SUMORIG=d42ad3eab739a2be8acc6d36dc99f6b2
 
 #echo -e "MD5SUM=$MD5SUM"
 #echo -e "MD5SUMORIG=$MD5SUMORIG"
